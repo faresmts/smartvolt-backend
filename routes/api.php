c27653bd7be138e821a49ac1955ed413c464e148
Matheus Fares <matheusfares@alu.ufc.br>
2026-01-05 23:52:26 -0300

    feat: devices and groups

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,16 @@
 <?php
 
+use App\Http\Controllers\Api\DeviceController;
+use App\Http\Controllers\Api\GroupController;
 use App\Http\Controllers\Api\LoginController;
 use App\Http\Controllers\Api\RegisterController;
 use Illuminate\Support\Facades\Route;
 
 Route::post('/register', RegisterController::class);
 Route::post('/login', LoginController::class);
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::apiResource('groups', GroupController::class);
+    Route::post('devices/link', [DeviceController::class, 'link']);
+    Route::apiResource('devices', DeviceController::class)->except(['store']);
+});
